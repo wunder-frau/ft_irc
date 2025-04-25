f580c8cf6fd569cb91897d7d476a63063e1b234d
walnut-ro <denlevinschi@gmail.com>
2025-04-25 12:23:31 +0300

    add support in Server case sensetive command

--- Server.cpp
+++ Server.cpp
@@ -22,21 +22,24 @@
 #include "utils.hpp"
 
 Server::Server(int port, std::string password, bool debugMode)
-    : _port(port), _password(password), _nextClientId(0), _debugMode(debugMode) {
+    : _port(port), _password(password), _nextClientId(0), _debugMode(debugMode)
+{
     _server_fd = socket(AF_INET, SOCK_STREAM, 0);
-    if (_server_fd < 0) {
+    if (_server_fd < 0)
+    {
         perror("socket");
         throw std::runtime_error("Failed to create socket");
     }
 
-    if (fcntl(_server_fd, F_SETFL, O_NONBLOCK) < 0) {
+    if (fcntl(_server_fd, F_SETFL, O_NONBLOCK) < 0)
+    {
         perror("fcntl");
         throw std::runtime_error("Failed to set socket to non-blocking");
     }
 
     int opt = 1;
-    if (setsockopt(_server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) <
-        0) {
+    if (setsockopt(_server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) < 0)
+    {
         perror("setsockopt");
         throw std::runtime_error("Failed to set socket options");
     }
@@ -46,12 +49,14 @@ Server::Server(int port, std::string password, bool debugMode)
     addr.sin_port = htons(_port);
     addr.sin_addr.s_addr = INADDR_ANY;
 
-    if (bind(_server_fd, (struct sockaddr*)&addr, sizeof(addr)) < 0) {
+    if (bind(_server_fd, (struct sockaddr*)&addr, sizeof(addr)) < 0)
+    {
         perror("bind");
         throw std::runtime_error("Failed to bind");
     }
 
-    if (listen(_server_fd, SOMAXCONN) < 0) {
+    if (listen(_server_fd, SOMAXCONN) < 0)
+    {
         perror("listen");
         throw std::runtime_error("Failed to listen");
     }
@@ -62,33 +67,40 @@ Server::Server(int port, std::string password, bool debugMode)
     _poll_fds.push_back(pfd);
 }
 
-Server::Server(const Server& other)
-    : _port(other.getPort()), _password(other.getPassword()) {}
+Server::Server(const Server& other) : _port(other.getPort()), _password(other.getPassword()) {}
 
-Server::~Server() {
+Server::~Server()
+{
     close(_server_fd);
     for (size_t i = 1; i < _poll_fds.size(); ++i) close(_poll_fds[i].fd);
 }
 
-Server& Server::operator=(const Server& other) {
-    if (this != &other) {
+Server& Server::operator=(const Server& other)
+{
+    if (this != &other)
+    {
         _port = other.getPort();
         _password = other.getPassword();
     }
     return *this;
 }
 
-void Server::run() {
+void Server::run()
+{
     std::cout << "Server running on port " << _port << std::endl;
-    while (true) {
+    while (true)
+    {
         int ret = poll(_poll_fds.data(), _poll_fds.size(), -1);
-        if (ret < 0) {
+        if (ret < 0)
+        {
             perror("poll");
             break;
         }
 
-        for (size_t i = 0; i < _poll_fds.size(); ++i) {
-            if (_poll_fds[i].revents & POLLIN) {
+        for (size_t i = 0; i < _poll_fds.size(); ++i)
+        {
+            if (_poll_fds[i].revents & POLLIN)
+            {
                 if (_poll_fds[i].fd == _server_fd)
                     acceptClient();
                 else
@@ -98,12 +110,14 @@ void Server::run() {
     }
 }
 
-void Server::acceptClient() {
+void Server::acceptClient()
+{
     sockaddr_in client_addr;
     socklen_t len = sizeof(client_addr);
     int client_fd = accept(_server_fd, (sockaddr*)&client_addr, &len);
 
-    if (client_fd < 0) {
+    if (client_fd < 0)
+    {
         perror("accept");
         return;
     }
@@ -126,10 +140,12 @@ void Server::acceptClient() {
     send(client_fd, welcome.c_str(), welcome.length(), 0);
 }
 
-void Server::receiveData(int clientFd, size_t index) {
+void Server::receiveData(int clientFd, size_t index)
+{
     char buffer[1024];
     ssize_t n = recv(clientFd, buffer, sizeof(buffer), 0);
-    if (n <= 0) {
+    if (n <= 0)
+    {
         std::cout << "[INFO] Client disconnected: fd=" << clientFd << std::endl;
         close(clientFd);
         _recvBuffers.erase(clientFd);
@@ -141,51 +157,62 @@ void Server::receiveData(int clientFd, size_t index) {
     auto& pending = _recvBuffers[clientFd];
     pending.append(buffer, n);
     size_t pos;
-    while ((pos = pending.find('\n')) != std::string::npos) {
+    while ((pos = pending.find('\n')) != std::string::npos)
+    {
         std::string line = pending.substr(0, pos);
         if (!line.empty() && line.back() == '\r')
             line.pop_back();
         pending.erase(0, pos + 1);
-        if (!isRegistered(clientFd)) {
+        if (!isRegistered(clientFd))
+        {
             registerClient(clientFd, line, &index);
-        } else {
+        }
+        else
+        {
             dispatchCommand(line, clientFd);
         }
     }
 }
 
-void Server::eraseClient(int clientFd, size_t* clientIndex) {
+void Server::eraseClient(int clientFd, size_t* clientIndex)
+{
     (void)clientIndex;
     debugLog("Erasing client with FD " + std::to_string(clientFd));
     // Find the client index
     size_t index = 0;
-    for (auto it = _clients.begin(); it != _clients.end(); ++it, ++index) {
-        if (it->getFd() == clientFd) {
+    for (auto it = _clients.begin(); it != _clients.end(); ++it, ++index)
+    {
+        if (it->getFd() == clientFd)
+        {
             // Remove the client from _clients vector
             _clients.erase(it);
-            
+
             // Update the client index
             if (clientIndex)
                 (*clientIndex)--;
-                
+
             break;
         }
     }
-    
+
     // Remove empty channels
     removeEmptyChannels();
 }
 
-bool Server::isRegistered(int clientFd) {
-    for (auto it = _clients.begin(); it != _clients.end(); ++it) {
+bool Server::isRegistered(int clientFd)
+{
+    for (auto it = _clients.begin(); it != _clients.end(); ++it)
+    {
         if (it->getFd() == clientFd)
             return it->isRegistered();
     }
     return false;
 }
 
-bool Server::isUniqueNick(std::string nick) {
-    for (auto it = _clients.begin(); it != _clients.end(); ++it) {
+bool Server::isUniqueNick(std::string nick)
+{
+    for (auto it = _clients.begin(); it != _clients.end(); ++it)
+    {
         if (it->getNick() == nick)
             return false;
     }
@@ -194,35 +221,42 @@ bool Server::isUniqueNick(std::string nick) {
 
 void Server::addClient(const Client& client) { _clients.push_back(client); }
 
-Client* Server::getClientObjByFd(int fd) {
-    for (auto& client : _clients) {
+Client* Server::getClientObjByFd(int fd)
+{
+    for (auto& client : _clients)
+    {
         if (client.getFd() == fd)
             return &client;
     }
     return nullptr;
 }
 
-Client* Server::getClientObjByNick(const std::string& nick) {
-    for (auto it = _clients.begin(); it != _clients.end(); ++it) {
+Client* Server::getClientObjByNick(const std::string& nick)
+{
+    for (auto it = _clients.begin(); it != _clients.end(); ++it)
+    {
         if (it->getNick() == nick)
             return &(*it);
     }
     return nullptr;
 }
 
-size_t Server::getClientIndex(int clientFd) {
-    for (size_t i = 0; i < _clients.size(); ++i) {
+size_t Server::getClientIndex(int clientFd)
+{
+    for (size_t i = 0; i < _clients.size(); ++i)
+    {
         if (_clients[i].getFd() == clientFd)
             return i;
     }
-    throw std::runtime_error("Client with fd " + std::to_string(clientFd) +
-                             " not found");
+    throw std::runtime_error("Client with fd " + std::to_string(clientFd) + " not found");
 }
 
-void Server::dispatchCommand(const std::string& fullMessage, int clientFd) {
+void Server::dispatchCommand(const std::string& fullMessage, int clientFd)
+{
     std::istringstream stream(fullMessage);
     std::string line;
-    while (std::getline(stream, line)) {
+    while (std::getline(stream, line))
+    {
         if (!line.empty() && line.back() == '\r')
             line.pop_back();
         if (!line.empty() && line[0] == ':')
@@ -233,29 +267,51 @@ void Server::dispatchCommand(const std::string& fullMessage, int clientFd) {
         if (tokens.empty())
             continue;
 
-        const std::string& command = tokens[0];
+        //        const std::string& command = tokens[0];
+        std::string command = toUpperCase(tokens[0]);
 
-        if (command == "NICK") {
+        if (command == "NICK")
+        {
             executeNick(*this, clientFd, line);
-        } else if (command == "JOIN") {
+        }
+        else if (command == "JOIN")
+        {
             handleJoin(clientFd, line);
-        } else if (command == "PART") {
+        }
+        else if (command == "PART")
+        {
             handlePart(clientFd, line);
-        } else if (command == "PRIVMSG" || command == "MSG") {
+        }
+        else if (command == "PRIVMSG" || command == "MSG")
+        {
             executePrivmsg(*this, clientFd, line);
-        } else if (command == "NOTICE") {
+        }
+        else if (command == "NOTICE")
+        {
             executeNotice(*this, clientFd, line);
-        } else if (command == "QUIT") {
+        }
+        else if (command == "QUIT")
+        {
             executeQuit(*this, clientFd, line);
-        } else if (command == "MODE") {
+        }
+        else if (command == "MODE")
+        {
             executeMode(*this, clientFd, line);
-        } else if (command == "TOPIC") {
+        }
+        else if (command == "TOPIC")
+        {
             handleTopic(clientFd, line);
-        } else if (command == "KICK") {
+        }
+        else if (command == "KICK")
+        {
             handleKick(clientFd, line);
-        } else if (command == "INVITE") {
+        }
+        else if (command == "INVITE")
+        {
             handleInvite(clientFd, line);
-        } else {
+        }
+        else
+        {
             sendError(clientFd, "421", command, ":Unknown command");
         }
     }
@@ -263,15 +319,15 @@ void Server::dispatchCommand(const std::string& fullMessage, int clientFd) {
 
 std::vector<Channel>& Server::getChannels() { return _channels; }
 
-Channel* Server::getChannelByName(const std::string& name) {
+Channel* Server::getChannelByName(const std::string& name)
+{
     std::string searchName = name;
-    while (!searchName.empty() && std::isspace(searchName.back()))
-        searchName.pop_back();
+    while (!searchName.empty() && std::isspace(searchName.back())) searchName.pop_back();
 
-    for (size_t i = 0; i < _channels.size(); ++i) {
+    for (size_t i = 0; i < _channels.size(); ++i)
+    {
         std::string channelName = _channels[i].getName();
-        while (!channelName.empty() && std::isspace(channelName.back()))
-            channelName.pop_back();
+        while (!channelName.empty() && std::isspace(channelName.back())) channelName.pop_back();
 
         if (channelName == searchName)
             return &_channels[i];
@@ -279,7 +335,8 @@ Channel* Server::getChannelByName(const std::string& name) {
     return nullptr;
 }
 
-Channel* Server::createOrGetChannel(const std::string& name) {
+Channel* Server::createOrGetChannel(const std::string& name)
+{
     std::string trimmed = name;
     while (!trimmed.empty() && std::isspace(trimmed.back())) trimmed.pop_back();
 
@@ -292,12 +349,13 @@ Channel* Server::createOrGetChannel(const std::string& name) {
 }
 
 // Helper method to safely disconnect a client
-void Server::handleClientDisconnect(int clientFd, size_t* clientIndex) {
+void Server::handleClientDisconnect(int clientFd, size_t* clientIndex)
+{
     std::cout << "Disconnecting client with FD " << clientFd << std::endl;
-    
+
     // First remove client from all channels
     removeClientFromChannels(clientFd);
-    
+
     // Then erase the client from the _clients vector
     eraseClient(clientFd, clientIndex);
 }
@@ -305,17 +363,21 @@ void Server::handleClientDisconnect(int clientFd, size_t* clientIndex) {
 #include <sstream>
 
 // Implementation of the parser function
-void Server::parser(std::string arg, std::vector<std::string>& params, char del) {
+void Server::parser(std::string arg, std::vector<std::string>& params, char del)
+{
     std::istringstream iss(arg);
     std::string token;
-    while (std::getline(iss, token, del)) {
+    while (std::getline(iss, token, del))
+    {
         if (!token.empty())
             params.push_back(token);
     }
 }
 
-void Server::debugLog(const std::string& msg) const {
-    if (_debugMode) {
+void Server::debugLog(const std::string& msg) const
+{
+    if (_debugMode)
+    {
         std::cout << "[DEBUG] " << msg << std::endl;
     }
 }
--- utils.cpp
+++ utils.cpp
@@ -1,47 +1,58 @@
 #include "utils.hpp"
-
-#include <sys/socket.h>  // for send()
-
-#include <sstream>
-
 #include "Server.hpp"
 
-void parser(const std::string& input, std::vector<std::string>& output,
-            char delimiter) {
+#include <sys/socket.h>  // for send()
+#include <sstream>
+#include <cctype>
+
+void parser(const std::string& input, std::vector<std::string>& output, char delimiter)
+{
     std::stringstream ss(input);
     std::string token;
-    while (std::getline(ss, token, delimiter)) {
+    while (std::getline(ss, token, delimiter))
+    {
         if (!token.empty())
             output.push_back(token);
     }
 }
 
-void sendError(Server& server, int clientFd, const std::string& errorCode,
-               const std::string& nick, const std::string& details) {
+void sendError(Server& server, int clientFd, const std::string& errorCode, const std::string& nick,
+               const std::string& details)
+{
     (void)server;  // currently unused
     sendError(clientFd, errorCode, nick, details);
 }
 
-void sendError(int clientFd, const std::string& errorCode,
-               const std::string& nick, const std::string& details) {
-    std::string msg =
-        ":ft_irc " + errorCode + " " + nick + " " + details + "\r\n";
+void sendError(int clientFd, const std::string& errorCode, const std::string& nick,
+               const std::string& details)
+{
+    std::string msg = ":ft_irc " + errorCode + " " + nick + " " + details + "\r\n";
     send(clientFd, msg.c_str(), msg.length(), 0);
 }
 
-std::string trimWhitespace(const std::string& str) {
+std::string trimWhitespace(const std::string& str)
+{
     std::string result = str;
     // Trim trailing whitespace
     while (!result.empty() && (result.back() == '\n' || result.back() == '\r' ||
-                               result.back() == ' ' || result.back() == '\t')) {
+                               result.back() == ' ' || result.back() == '\t'))
+    {
         result.pop_back();
     }
     // Trim leading whitespace (if needed)
     size_t start = 0;
-    while (start < result.length() &&
-           (result[start] == ' ' || result[start] == '\t' ||
-            result[start] == '\n' || result[start] == '\r')) {
+    while (start < result.length() && (result[start] == ' ' || result[start] == '\t' ||
+                                       result[start] == '\n' || result[start] == '\r'))
+    {
         ++start;
     }
     return (start > 0) ? result.substr(start) : result;
+}
+
+std::string toUpperCase(const std::string& str)
+{
+    std::string result = str;
+    for (size_t i = 0; i < result.size(); ++i)
+        result[i] = std::toupper(static_cast<unsigned char>(result[i]));
+    return result;
 }
